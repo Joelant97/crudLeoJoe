4252d4dae378f3a0365ca9edde69dc84442e0ef9
Joel Antonio Rodriguez <j rodriguez@CS-CESAR1.kdomain.com>
2020-05-22 23:53:39 -0400

    Initial commit

---
+++ src/main/java/com/pucmm/crudleojoe/CrudleojoeApplication.java
@@ -0,0 +1,13 @@
+package com.pucmm.crudleojoe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudleojoeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CrudleojoeApplication.class, args);
+    }
+
+}
